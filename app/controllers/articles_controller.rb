0f6dd33f4ae7d9ec6f14e3c80e89f6087bc45b20
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-29 16:20:51 -0400

    Adds security to credentials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=keyword&apiKey=YOUR-API-KEY")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=keyword&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
     render json: response.parse(:json)
   end
 end
